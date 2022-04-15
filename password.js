6de9e33d0743e4713060a5f1b0a23e12c015d38f
Andrew Petersen <fespetersen1990@gmail.com>
2022-04-15 11:33:59 -0600

    Password Validator working

---
+++ password.js
@@ -0,0 +1,22 @@
+const readline = require('readline');
+
+const reader = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+let welcome = "Welcome to the Student Password Validator!"
+console.log(welcome);
+
+reader.question("Please enter the password you would like to validate.", function(password){
+    if (password.length >= 10) {
+        console.log("Your password is long enough.");
+    } else if (password.length < 10) {
+        console.log("Your password is too short.");
+    };
+
+
+
+    reader.close()
+})
+
